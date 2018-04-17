e5cd819254597059391b259221c38fdc5554c44e
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2018-04-17 18:09:20 -0500

    added redux stuff

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//redux stuffs
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux';
+import reducers from './redux/rootReducer';
+import ReduxThunk from 'redux-thunk';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore);
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 registerServiceWorker();
