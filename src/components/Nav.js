44b4cc624cabe2c9f44904ca2f92221f43b1a194
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2018-04-18 00:35:35 -0500

    refactor: make nave cleaner and add title icon

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import title from '../assets/Title.png'
 const Nav =() => {
     return (
         <header className="App-header">
-            <div style={{display: 'flex', flexDirection: 'row', padding: '20px', justifyContent: 'space-evenly'}}>
-                <div><Link to="/">Menu</Link></div>
-                <div><Link to="/orders">Orders</Link></div>
-            </div>
+                <div style={{display: 'flex', flexDirection: 'row', padding: '20px', justifyContent: 'space-evenly'}}>
+                    <img src={title} alt={'WaitLis'} height={200} width={200}/>
+                    <Link to="/"><button className={'btn btn-primary'}>Menu</button></Link>
+                    <Link to="/orders"><button className={'btn btn-primary'}>Orders</button></Link>
+                </div>
         </header>
     )
 };
