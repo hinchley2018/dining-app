e1a38be4f5ca8b617e88277affb64decb88472ed
Jonathon Hinchley <dev.jjh@planet-hinchley.com>
2018-04-18 00:36:46 -0500

    fix: some typos and refactor how things are added

--- src/redux/orders.js
+++ src/redux/orders.js
@@ -3,7 +3,7 @@ import {API_URL} from '../config';
 
 // Types
 const GET_MENU_ITEMS = 'get_menu_items',
-    ADD_ITEMS = 'add_items',
+    ADD_ITEM = 'add_item',
     REMOVE_ITEM = 'remove_item',
     CONFIRM_ORDER = 'confirm_order',
     CANCEL_ORDER = 'cancel_order',
@@ -18,8 +18,8 @@ export default function reducer(state = INITIAL_STATE, action = {}) {
     switch (action.type) {
         case GET_MENU_ITEMS:
             return {...state, menuItems: action.items};
-        case ADD_ITEMS:
-            return {...state, cart: action.items};
+        case ADD_ITEM:
+            return {...state, cart: [...state.cart, action.item]};
         case REMOVE_ITEM:
             return {...state,
                 cart: [
@@ -43,17 +43,33 @@ export default function reducer(state = INITIAL_STATE, action = {}) {
 // Action Creators
 export function getItems() {
     return function(dispatch){
+        //TODO: remove this once I get api
+        const items = [
+            {
+                title: 'Chicken Sandwich',
+                price: '6.75'
+            },
+            {
+                title: 'Spicy Deluxe',
+                price: '6.75'
+            },
+            {
+                title: '8 Ct. Chicken Nuggets',
+                price: '6.75'
+            }
+        ]
+        dispatch({ type: GET_MENU_ITEMS, items: items});
         axios.get(`${API_URL}/items`)
             .then(response => {
-                dispatch({ type: GET_MENU_ITEMS, items: response.data});
+
             })
 
     };
 }
 
-export function addItems(itemsList) {
+export function addItem(item) {
     return function(dispatch){
-        dispatch({ type: ADD_ITEMS, items: itemsList});
+        dispatch({ type: ADD_ITEM, item: item});
     };
 }
 
